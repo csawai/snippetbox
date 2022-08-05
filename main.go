bb8d84b1a6b5f05ed1c527c1c3542c6c5663fa43
csawai <csawai@gmail.com>
2022-08-05 16:10:32 +0530

    add id query conditions

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 //home function is the route to the home page. This will get triggered when main function is called.  We want to show some
@@ -23,16 +24,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("this is a show box"))
 
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specif id", id)
+
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+		w.Header().Set("allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method not allowed"))
 		return
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("this is for creating a snippet box"))
 }
 
